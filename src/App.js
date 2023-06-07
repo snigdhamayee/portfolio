ef9a317b034092335c5ed99a6bb06c2f2ea165a7
snigdhamayee sahoo <snigdhamayeesahoobbnk@gmail.com>
2023-06-07 20:15:05 +0530

    portfolio setup

---
+++ src/App.js
@@ -0,0 +1,27 @@
+
+import './App.css';
+import { Routes, Route } from 'react-router-dom'
+import Home from "./containers/home"
+import About from "./containers/about"
+import Resume from "./containers/resume"
+import Skills from "./containers/skills"
+import Portfolio from "./containers/portfolio"
+import Contact from "./containers/contact"
+function App() {
+  return (
+    <div className="App">
+
+      <Routes>
+        <Route index path='/' element={<Home />} />
+        <Route index path='/about' element={<About/>} />
+        <Route index path='/resume' element={<Resume/>} />
+        <Route index path='/skills' element={<Skills/>} />
+        <Route index path='/portfolio' element={<Portfolio/>} />
+        <Route index path='/contact' element={<Contact/>} />
+        </Routes>
+      
+    </div>
+  );
+}
+
+export default App;
